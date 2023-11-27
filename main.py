cf32f817527b505746c011104c4f32c30cd27527
GopiRanganathan <gopiranga9@gmail.com>
2023-11-27 14:05:13 +0530

    make correction

--- main.py
+++ main.py
@@ -249,14 +249,17 @@ def about():
     return render_template("about.html", logged_in=current_user.is_authenticated)
 
 async def send_email(msg):
-    async with smtplib.SMTP("smtp.gmail.com") as connection:
-            # await connection.connect()
-           connection.starttls()
-           connection.login(user=os.environ.get("FROM_EMAIL"), password=os.environ.get("PASSWORD"))
-           connection.sendmail(from_addr=os.environ.get("FROM_EMAIL"), 
-                                to_addrs=os.environ.get("TO_EMAIL"),
-                                msg=f"Subject: Someone wants to get in touch with you!\n\n{msg}")   
-    await asyncio.sleep(3)
+    try:
+        async with SMTP("smtp.gmail.com") as connection:
+                await connection.connect()
+                await connection.starttls()
+                await connection.login(user=os.environ.get("FROM_EMAIL"), password=os.environ.get("PASSWORD"))
+                await connection.sendmail(from_addr=os.environ.get("FROM_EMAIL"), 
+                                    to_addrs=os.environ.get("TO_EMAIL"),
+                                    msg=f"Subject: Someone wants to get in touch with you!\n\n{msg}")   
+                await asyncio.sleep(3)
+    except Exception as e:
+        print(f"Failed to send email: {e}")
     print("msg sent")
 
 @app.route("/contact", methods =['POST', 'GET'])
@@ -269,7 +272,7 @@ def contact():
         message=request.form['message']
         msg_to_send = f'Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}'
         # print(msg_to_send)
-        asyncio.run(send_email(msg_to_send))
+        asyncio.create_task(send_email(msg_to_send))
         flash('Message sent successfully!')
         return redirect(url_for('contact'))
     
